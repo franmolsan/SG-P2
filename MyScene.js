4de52ac54199ae6d569ebcfa67e0967501967eb4
franmolsan <56502397+franmolsan@users.noreply.github.com>
2020-06-02 17:42:34 +0200

    inicio con cannon

--- MyScene.js
+++ MyScene.js
@@ -1,54 +1,58 @@
-
 /// La clase fachada del modelo
 /**
  * Usaremos una clase derivada de la clase Scene de Three.js para llevar el control de la escena y de todo lo que ocurre en ella.
  */
 
 class MyScene extends THREE.Scene {
-  constructor (myCanvas) {
+  constructor(myCanvas) {
     super();
 
     // Lo primero, crear el visualizador, pasándole el lienzo sobre el que realizar los renderizados.
     this.renderer = this.createRenderer(myCanvas);
 
     // Se añade a la gui los controles para manipular los elementos de esta clase
-    this.gui = this.createGUI ();
+    this.gui = this.createGUI();
 
     // Construimos los distinos elementos que tendremos en la escena
 
     // Todo elemento que se desee sea tenido en cuenta en el renderizado de la escena debe pertenecer a esta. Bien como hijo de la escena (this en esta clase) o como hijo de un elemento que ya esté en la escena.
     // Tras crear cada elemento se añadirá a la escena con   this.add(variable)
-    this.createLights ();
+    this.createLights();
 
     // Tendremos una cámara con un control de movimiento con el ratón
-    this.createCamera ();
+    this.createCamera();
 
     // Y unos ejes. Imprescindibles para orientarnos sobre dónde están las cosas
-    this.axis = new THREE.AxesHelper (5);
-    this.add (this.axis);
-
+    this.axis = new THREE.AxesHelper(5);
+    this.add(this.axis);
 
     this.nave = new Camino();
     this.add(this.nave);
-
   }
 
-
-  createCamera () {
+  createCamera() {
     // Para crear una cámara le indicamos
     //   El ángulo del campo de visión en grados sexagesimales
     //   La razón de aspecto ancho/alto
     //   Los planos de recorte cercano y lejano
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
     // También se indica dónde se coloca
-    this.camera.position.set (20, 10, 20);
+    this.camera.position.set(20, 10, 20);
     // Y hacia dónde mira
-    var look = new THREE.Vector3 (0,0,0);
+    var look = new THREE.Vector3(0, 0, 0);
     this.camera.lookAt(look);
-    this.add (this.camera);
+    this.add(this.camera);
 
     // Para el control de cámara usamos una clase que ya tiene implementado los movimientos de órbita
-    this.cameraControl = new THREE.TrackballControls (this.camera, this.renderer.domElement);
+    this.cameraControl = new THREE.TrackballControls(
+      this.camera,
+      this.renderer.domElement
+    );
     // Se configuran las velocidades de los movimientos
     this.cameraControl.rotateSpeed = 5;
     this.cameraControl.zoomSpeed = -2;
@@ -57,58 +61,59 @@ class MyScene extends THREE.Scene {
     this.cameraControl.target = look;
   }
 
-
-  createGUI () {
+  createGUI() {
     // Se crea la interfaz gráfica de usuario
     var gui = new dat.GUI();
 
     // La escena le va a añadir sus propios controles.
     // Se definen mediante una   new function()
     // En este caso la intensidad de la luz y si se muestran o no los ejes
-    this.guiControls = new function() {
+    this.guiControls = new (function () {
       // En el contexto de una función   this   alude a la función
       this.lightIntensity = 0.5;
       this.axisOnOff = true;
-    }
+    })();
 
     // Se crea una sección para los controles de esta clase
-    var folder = gui.addFolder ('Luz y Ejes');
+    var folder = gui.addFolder("Luz y Ejes");
 
     // Se le añade un control para la intensidad de la luz
-    folder.add (this.guiControls, 'lightIntensity', 0, 1, 0.1).name('Intensidad de la Luz : ');
+    folder
+      .add(this.guiControls, "lightIntensity", 0, 1, 0.1)
+      .name("Intensidad de la Luz : ");
 
     // Y otro para mostrar u ocultar los ejes
-    folder.add (this.guiControls, 'axisOnOff').name ('Mostrar ejes : ');
+    folder.add(this.guiControls, "axisOnOff").name("Mostrar ejes : ");
 
     return gui;
   }
 
-  createLights () {
+  createLights() {
     // Se crea una luz ambiental, evita que se vean complentamente negras las zonas donde no incide de manera directa una fuente de luz
     // La luz ambiental solo tiene un color y una intensidad
     // Se declara como   var   y va a ser una variable local a este método
     //    se hace así puesto que no va a ser accedida desde otros métodos
     var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
     // La añadimos a la escena
-    this.add (ambientLight);
+    this.add(ambientLight);
 
     // Se crea una luz focal que va a ser la luz principal de la escena
     // La luz focal, además tiene una posición, y un punto de mira
     // Si no se le da punto de mira, apuntará al (0,0,0) en coordenadas del mundo
     // En este caso se declara como   this.atributo   para que sea un atributo accesible desde otros métodos.
-    this.spotLight = new THREE.SpotLight( 0xffffff, 0.5 );
-    this.spotLight.position.set( 0, 60, 0 );
-    this.add (this.spotLight);
+    this.spotLight = new THREE.SpotLight(0xffffff, 0.5);
+    this.spotLight.position.set(0, 60, 0);
+    this.add(this.spotLight);
   }
 
-  createRenderer (myCanvas) {
+  createRenderer(myCanvas) {
     // Se recibe el lienzo sobre el que se van a hacer los renderizados. Un div definido en el html.
 
     // Se instancia un Renderer   WebGL
     var renderer = new THREE.WebGLRenderer();
 
     // Se establece un color de fondo en las imágenes que genera el render
-    renderer.setClearColor(new THREE.Color(0xEEEEEE), 1.0);
+    renderer.setClearColor(new THREE.Color(0xeeeeee), 1.0);
 
     // Se establece el tamaño, se aprovecha la totalidad de la ventana del navegador
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -119,13 +124,13 @@ class MyScene extends THREE.Scene {
     return renderer;
   }
 
-  getCamera () {
+  getCamera() {
     // En principio se devuelve la única cámara que tenemos
     // Si hubiera varias cámaras, este método decidiría qué cámara devuelve cada vez que es consultado
     return this.camera;
   }
 
-  setCameraAspect (ratio) {
+  setCameraAspect(ratio) {
     // Cada vez que el usuario modifica el tamaño de la ventana desde el gestor de ventanas de
     // su sistema operativo hay que actualizar el ratio de aspecto de la cámara
     this.camera.aspect = ratio;
@@ -133,21 +138,21 @@ class MyScene extends THREE.Scene {
     this.camera.updateProjectionMatrix();
   }
 
-  onWindowResize () {
+  onWindowResize() {
     // Este método es llamado cada vez que el usuario modifica el tamapo de la ventana de la aplicación
     // Hay que actualizar el ratio de aspecto de la cámara
-    this.setCameraAspect (window.innerWidth / window.innerHeight);
+    this.setCameraAspect(window.innerWidth / window.innerHeight);
 
     // Y también el tamaño del renderizador
-    this.renderer.setSize (window.innerWidth, window.innerHeight);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  update () {
+  update() {
     // Este método debe ser llamado cada vez que queramos visualizar la escena de nuevo.
 
     // Literalmente le decimos al navegador: "La próxima vez que haya que refrescar la pantalla, llama al método que te indico".
     // Si no existiera esta línea,  update()  se ejecutaría solo la primera vez.
-    requestAnimationFrame(() => this.update())
+    requestAnimationFrame(() => this.update());
 
     // Se actualizan los elementos de la escena para cada frame
     // Se actualiza la intensidad de la luz con lo que haya indicado el usuario en la gui
@@ -163,19 +168,67 @@ class MyScene extends THREE.Scene {
     //TWEEN.update();
 
     // Le decimos al renderizador "visualiza la escena que te indico usando la cámara que te estoy pasando"
-    this.renderer.render (this, this.getCamera());
+    this.renderer.render(this, this.getCamera());
   }
 }
 
 /// La función   main
 $(function () {
-
   // Se instancia la escena pasándole el  div  que se ha creado en el html para visualizar
   var scene = new MyScene("#WebGL-output");
 
   // Se añaden los listener de la aplicación. En este caso, el que va a comprobar cuándo se modifica el tamaño de la ventana de la aplicación.
-  window.addEventListener ("resize", () => scene.onWindowResize());
+  window.addEventListener("resize", () => scene.onWindowResize());
 
   // Que no se nos olvide, la primera visualización.
   scene.update();
 });
+
+function iniciarCanon() {
+
+  // Crear el mundo
+  world = new CANON.World();
+  world.quatNormalizeSkip = 0;
+  world.quatNormalizeFast = false;
+
+  var solver = new CANNON.GSSolver();
+
+  world.defaultContactMaterial.contactEquationStiffness = 1e9;
+  world.defaultContactMaterial.contactEquationRelaxation = 4;
+
+  solver.iterations = 7;
+  solver.tolerance = 0.1;
+  var split = true;
+  if(split)
+      world.solver = new CANNON.SplitSolver(solver);
+  else
+      world.solver = solver;
+
+  world.gravity.set(0,-20,0);
+  world.broadphase = new CANNON.NaiveBroadphase();
+
+  // Create a slippery material (friction coefficient = 0.0)
+  physicsMaterial = new CANNON.Material("slipperyMaterial");
+  var physicsContactMaterial = new CANNON.ContactMaterial(physicsMaterial, physicsMaterial,
+                                                          0.0, // friction coefficient
+                                                          0.3);  // restitution
+  // We must add the contact materials to the world
+  world.addContactMaterial(physicsContactMaterial);
+
+  // Create a sphere
+  var mass = 5, radius = 1.3;
+  sphereShape = new CANNON.Sphere(radius);
+  sphereBody = new CANNON.Body({ mass: mass });
+  sphereBody.addShape(sphereShape);
+  sphereBody.position.set(0,5,0);
+  sphereBody.linearDamping = 0.9;
+  world.addBody(sphereBody);
+
+  // Create a plane
+  var groundShape = new CANNON.Plane();
+  var groundBody = new CANNON.Body({ mass: 0 });
+  groundBody.addShape(groundShape);
+  groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+  world.addBody(groundBody);
+
+}
